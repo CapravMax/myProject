2d6df0ae7b9b54316a98f095b26928c0cab9b3ed
Maxim Tsvetkov <65393034+CapravMax@users.noreply.github.com>
2023-02-14 12:20:42 +0300

    Main +Slider

--- src/components/Slider/SlideList.jsx
+++ src/components/Slider/SlideList.jsx
@@ -1,1 +1,20 @@
+import React, {useContext} from "react";
+import Slide from './Slider';
+import { SliderContext } from "./Slider";
 
+import "./SlideList"
+
+export default function SlidesList () {
+    const {slideNumber, items} = useContext (SliderContext);
+
+    return (
+
+        <div className="slide-list"
+        style = {{transform: `translateX (-${slideNumber*100}%)`}}>
+
+            {items.map((slide, index) =>(
+                <Slide key={index} data = {slide}/>
+            ))}
+        </div>
+    )
+}
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState, createContext} from "react";
 import Dots from './Dots';
 import SlidesList from './SlideList';
 
+import "./Slider.scss"
+
 
 export const SliderContext = createContext();
 const Slider = function ({width, height, autoPlay, autoPlayTime}) {
@@ -9,6 +11,106 @@ const Slider = function ({width, height, autoPlay, autoPlayTime}) {
     const [slide, setSlide] = useState (0);
     const [touchPosition, setTouchPosition] = useState (null);
 
+useEffect (() => {
+    const loadData = async () => {
+        const images = await getImages ();
+        setItems (images);
 
+    }
+    loadData ();
+
+}, [])
+
+const changeSlide = (direction = 1) => {
+    let slideNumber = 0;
+
+    if (slide + direction <0) {
+        slideNumber = items.length-1;}
+
+        else {
+            slideNumber = (slide + direction) % items.length
+        }
+    
+    setSlide (slideNumber);
+
+};
+
+const goToSlide = (number) => {
+    setSlide (number % items.length);
+};
+
+const  handleTouchStart = (e) => {
+
+    const touchDown =e.touches[0].clientX;
+    
+    setTouchPosition (touchDown)
+};
+
+const handleTouchMove = (e) => {
+
+    if (touchPosition===null){
+        return;
+    }
+
+    const  currentPosition = e.touches[0].clientX;
+    const direction = touchPosition - currentPosition;
+
+    if (direction > 10) {
+        changeSlide (1);
+    }
+
+    if (direction <-10) {
+        changeSlide (-1)
+    }
+    setTouchPosition (null)
 }
+
+useEffect(() => {
+    if (! autoPlay) 
+    return;
+
+    const interval = setInterval (() => {
+        changeSlide (1);
+    }, autoPlayTime);
+
+    return ( ) => {
+        clearInterval (interval);
+    };
+    }, [items.length, slide]);
+
+    return (
+
+       <div style = {{width, height}} 
+       className = 'slider' 
+       oneTouchStart = {handleTouchStart} 
+       onTouchMove = {handleTouchMove}>
+
+<SliderContext.Provider value = {{
+    goToSlide, changeSlide,
+    slidesCount: items.length,
+    slideNumber: slide,
+    items, }}>
+        <SlidesList/>
+        <Dots/>
+    </SliderContext.Provider>
+
+       </div>
+    );
+};
+
+Slider.propTypes =  {
+    autoPlay: PropTypes.bool,
+    autoPlayTime: PropTypes.number,
+    width: PropTypes.string,
+    height: PropTypes.string
+};
+
+Slider.defaultProps = {
+    autoPlay: false,
+    autoPlayTime: 5000,
+    width: '100%',
+    height: '100%'
+}
+
+export default Slider;
 
